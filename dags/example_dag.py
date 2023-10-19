4f39fae18488b59a70d0f1f8b8a21e293c0e83eb
Eric Schwartz <eschwartz1991@gmail.com>
2023-10-19 13:46:39 +0000

    unchain

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -59,5 +59,4 @@ with models.DAG(
     )
 
     # Define DAG dependencies.
-    print_dag_run_conf >> print_a_fake_secret
-    print_a_fake_secret >> [print_a_fake_secret_another_way, print_a_fake_secret_third_way]
+    print_dag_run_conf >> [print_a_fake_secret, print_a_fake_secret_another_way, print_a_fake_secret_third_way]
